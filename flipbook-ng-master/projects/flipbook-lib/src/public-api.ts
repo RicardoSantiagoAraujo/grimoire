dc86ab34a02d2a855eaccab13f57517fd3f17126
ricardoSA54 <ricardosa54@gmail.com>
2023-12-21 14:29:42 +0000

    delete old template

--- flipbook-ng-master/projects/flipbook-lib/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of flipbook
- */
-
-export * from './lib/interfaces';
-export * from './lib/book/book.component';
-export * from './lib/flipbook.service';
-export * from './lib/flipbook.module';
