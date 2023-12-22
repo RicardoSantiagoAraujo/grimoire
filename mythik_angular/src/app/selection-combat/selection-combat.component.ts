132c6b27ef55a1d3c9d695e9cf8bf72030261dca
bmoegle <baptiste.moegle@gmail.com>
2023-12-22 17:56:08 +0100

    selectioncombat v0.8

--- mythik_angular/src/app/selection-combat/selection-combat.component.ts
+++ mythik_angular/src/app/selection-combat/selection-combat.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CreatureService } from '../creature/creature.service';
-import { Combattant, Creature } from '../model';
+import { Combattant, Creature, IA } from '../model';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 
@@ -12,8 +12,10 @@ import { Router } from '@angular/router';
 export class SelectionCombatComponent {
 
   creatures$!: Observable<Creature[]>;
-  combattant1?: Combattant; 
-  combattant2?: Combattant; 
+  creatures?: Creature[]; 
+  ia?: IA;
+  combattant1?: Combattant | undefined; 
+  combattant2?: Combattant | undefined; 
 
   constructor(private creatureService: CreatureService, private router: Router){
     this.load(); 
@@ -21,26 +23,32 @@ export class SelectionCombatComponent {
 
   load() {
     this.creatures$ = this.creatureService.findAll();
+    this.creatures$.subscribe(resp => {this.creatures=resp})
+    
   }
 
   list() {
     return this.creatures$;
   }
 
-  creationCombattant1(){
+  // creationCombattant1(){
+  //     this.combattant1.joueur = 
 
-
-  }
-  /*
-  creationCombattant2(){
-    let nb = Math.floor(Math.random() * (this.creatures$.subscribe(resp=> {resp.size()})))
-
+  // }
+  
+  // creationCombattant2(){
     
-  }
-*/
-  goCombat(){
+  //   let nb = Math.floor(Math.random() * (this.creatures!.length +1))
+  //   this.combattant2!.creature = this.creatures![nb];
+  //   this.combattant2!.joueur = this.ia; 
 
+
+  // }
+
+  goCombat(){
+    //this.creationCombattant2(); 
     this.router.navigate(['/combat']);
+   
   }
 
 }
