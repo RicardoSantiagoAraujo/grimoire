6812a939edf23b37fb4b212af68d98763e3c3224
ricardoSA54 <ricardosa54@gmail.com>
2024-01-03 21:32:45 +0100

    debut animations entre routes

---
+++ mythik_angular/src/app/chains/chains.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chains',
+  templateUrl: './chains.component.html',
+  styleUrls: ['./chains.component.css']
+})
+export class ChainsComponent {
+
+}
