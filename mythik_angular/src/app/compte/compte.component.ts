8d7c5f43d81eb1492bd2b62a571f36db548a00a7
ricardoSA54 <ricardosa54@gmail.com>
2024-01-14 12:49:03 +0100

    table sorting functionality

--- mythik_angular/src/app/compte/compte.component.ts
+++ mythik_angular/src/app/compte/compte.component.ts
@@ -29,11 +29,11 @@ export class CompteComponent {
   ngOnInit(): void {
     this.idCtrl = this.formBuilder.control('');
     this.emailCtrl = this.formBuilder.control('');
-    this.loginCtrl = this.formBuilder.control('',Validators.required);
+    this.loginCtrl = this.formBuilder.control('', Validators.required);
     this.passwordCtrl = this.formBuilder.control('');
     this.typeCtrl = this.formBuilder.control('');
 
-    this.compteForm = this.formBuilder.group( {
+    this.compteForm = this.formBuilder.group({
       id: this.idCtrl,
       email: this.emailCtrl,
       login: this.loginCtrl,
@@ -75,7 +75,7 @@ export class CompteComponent {
     this.compteService.save(newcompte).subscribe(resp => {
       this.compteForm.patchValue(resp);
       this.load();
-      })
+    })
     this.cancel();
   }
 
@@ -88,14 +88,72 @@ export class CompteComponent {
     this.compteForm.reset();
   }
 
-  retour(){
+  retour() {
     this.router.navigate(['/accueil']);
     this.exitAnimation()
 
   }
 
-    // add class to trigger exit animation on click
-    exitAnimation(){
-      document.querySelector("section")?.classList.add("exitAnimation");
+  // add class to trigger exit animation on click
+  exitAnimation() {
+    document.querySelector("section")?.classList.add("exitAnimation");
+  }
+
+
+  // Function to sort table by column
+  sortTable(n: number) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("table_compte")! as HTMLTableElement;
+    switching = true;
+    //Set the sorting direction to ascending:
+    dir = "asc";
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+      //start by saying: no switching is done:
+      switching = false;
+      rows = table.rows;
+      /*Loop through all table rows (except the
+      first, which contains table headers):*/
+      for (i = 1; i < (rows.length - 1); i++) {
+        //start by saying there should be no switching:
+        shouldSwitch = false;
+        /*Get the two elements you want to compare,
+        one from current row and one from the next:*/
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 1].getElementsByTagName("TD")[n];
+        /*check if the two rows should switch place,
+        based on the direction, asc or desc:*/
+        if (dir == "asc") {
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            //if so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            //if so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+      if (shouldSwitch) {
+        /*If a switch has been marked, make the switch
+        and mark that a switch has been done:*/
+        rows[i].parentNode!.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        //Each time a switch is done, increase this count by 1:
+        switchcount++;
+      } else {
+        /*If no switching has been done AND the direction is "asc",
+        set the direction to "desc" and run the while loop again.*/
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
     }
+  }
+
 }
