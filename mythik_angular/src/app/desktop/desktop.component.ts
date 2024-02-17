3da0db438b7716a81a3712c43b5e6a8cf418bad6
ricardoSA54 <ricardosa54@gmail.com>
2024-02-17 12:34:32 +0100

    added background insect

--- mythik_angular/src/app/desktop/desktop.component.ts
+++ mythik_angular/src/app/desktop/desktop.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation, Input } from '@angular/core';
+import { Component, ViewEncapsulation, Input, OnInit } from '@angular/core';
 import {Router} from "@angular/router"
 
 @Component({
@@ -8,10 +8,13 @@ import {Router} from "@angular/router"
   // encapsulation: ViewEncapsulation.None, // Use None to disable encapsulation and be able to style generated elements
 })
 
-export class DesktopComponent {
+export class DesktopComponent implements OnInit {
   exitGrimoire: boolean = false;
 
   constructor(private router: Router) { }
+  ngOnInit(): void {
+    this.loop()
+  }
 
   // add class to trigger exit animation on click
   exitAnimation(){
@@ -24,4 +27,33 @@ export class DesktopComponent {
         1000
         )
   }
+
+  // insect animation
+  insectAnimate(speed:number){
+    Math.random() * 1000 * 60;
+    let insect_track = document.querySelector<HTMLElement>("#insect_track")!;
+    let insect = document.querySelector<HTMLElement>("#insect_track img")!;
+    let random_angle =  Math.round(Math.random() * 360);
+    insect_track.style.transform = `translate(-50%,-50%) rotate(${random_angle}deg)`;
+    insect.style.transition = "right " + speed/1000 + "s";
+    if (insect.style.right == "0%"){ // GO BACK
+      insect.style.right= "100%";
+      insect.style.transform= "scaleX(1)";
+    } else { // GO FORTH
+      insect.style.right= "0%";
+      insect.style.transform= "scaleX(-1)";
+    }
+  }
+
+  // triggeranimation at set intervals
+  loop() {
+    let min_gap = 6000; // minimum amount of milisecs between animations
+    let max_gap = 1000 * 3; // max amount of milisects between animation ( + min_gap)
+    var rand = Math.round(Math.random() * max_gap) + min_gap;
+    setTimeout(() => {
+            this.insectAnimate(min_gap);
+            this.loop();
+            console.log("insect !")
+    }, rand);
+  };
 }
